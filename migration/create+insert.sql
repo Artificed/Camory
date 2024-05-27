16b0eb0237073bd8d9104d0c211d341180bbbdf2
Guardstyle <littlefamous9@gmail.com>
2024-05-28 01:08:18 +0700

    add navbar, need improvement on design

--- migration/create+insert.sql
+++ migration/create+insert.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
     id CHAR(36) PRIMARY KEY NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL
 );
 
 INSERT INTO users(id, email, password, username)
